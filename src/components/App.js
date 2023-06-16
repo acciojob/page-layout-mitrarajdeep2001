c12d9ac6b0e8bf504f47fcb88a0c80186f24bc73
Rajdeep Mitra <116889292+mitrarajdeep2001@users.noreply.github.com>
2023-06-16 10:06:56 +0000

    Initial commit

--- src/components/App.js
+++ src/components/App.js
@@ -1,11 +1,16 @@
 
 import React from "react";
 import './../styles/App.css';
+import Header from "./Header";
+import Children from "./Children";
+import Footer from "./Footer";
 
 const App = () => {
   return (
     <div>
-        {/* Do not remove the main div */}
+        <Header header={'Welcome to my website'}/>
+        <Children child={'This is the content of my website.'}/>
+        <Footer>&#169; 2023 My Website. All rights reserved.</Footer>
     </div>
   )
 }
